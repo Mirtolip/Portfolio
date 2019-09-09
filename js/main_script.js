21c77fb81b8f7afad4eddec214908ec92ca778b3
[DMirtolip <Dragon53050@gmail.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[d>
2019-09-09 07:16:57 +0500

    Portfolio FabosGold first commit

---
+++ js/main_script.js
@@ -0,0 +1,46 @@
+// Navigation scroll
+
+$(function () {
+  $(document).scroll(function () {
+	  var $nav = $("#header");
+	  $nav.toggleClass('background_color', $(this).scrollTop() > $nav.height());
+	});
+});
+
+
+// Carousel
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("header_background");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 5000); // Change image every 5 seconds
+}
+
+
+// Tab content
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+
